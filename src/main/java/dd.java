ceb1cd0e7a0b64d0276b74c4d1ab6796e512ac38
xiezhanchun <2388381447@qq.com>
2022-08-12 21:45:54 +0800

    ho-fix  commit

--- src/main/java/dd.java
+++ src/main/java/dd.java
@@ -3,5 +3,6 @@ public class dd {
     public static void main(String[] args) {
         System.out.println("hello  world");
         System.out.println("hello  world2");
+        System.out.println("hello  hot-fix");
     }
 }
