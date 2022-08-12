6a01e7c6afeba6a8ef5c768db0208f02bcff28fd
xiezhanchun <2388381447@qq.com>
2022-08-13 04:00:11 +0800

    Merge branch 'hot-fix'
    # Conflicts:
    #	src/main/java/dd.java

--- src/main/java/dd.java
+++ src/main/java/dd.java
@@ -7,5 +7,6 @@ public class dd {
         System.out.println("hello  main  text");
         System.out.println("hello  hot-fix  text");
         System.out.println("hello  push  text");
+        System.out.println("hello  push  text");
     }
 }
